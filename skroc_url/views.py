edb02ae680600704fe758bbb034d64584327012b
DominikZurawski <dominikzurawski15@gmail.com>
2019-06-29 11:09:38 +0200

    zmiana widoku

--- skroc_url/views.py
+++ skroc_url/views.py
@@ -10,3 +10,22 @@ def home_view(request,*args,**kwargs):
 	#return HttpResponse("<h1>Hello World</h1>")
 	context ={"title" : "ZADANIE Rekrutacyjne:"}
 	return render(request,"skroc_url/home.html",context)
+
+def index(request):
+	return render(request, 'skroc_url/index.html', context)
+
+def inputURL_view(request):   
+	form = InputURLForm(request.POST or None)
+	if form.is_valid():
+		OutputURLForm.shortURL = form.save(commit=False)
+		link = form.save(commit=False)
+		form.save()
+		form = InputURLForm
+		return render(request, 'skroc_url/outputURL.html', {"link": OutputURLForm.shortURL,"info" : "Twój link! "})
+	else: 
+		form = InputURLForm()		
+			
+	context = {"description" : "Wklej adres URL:",
+			   "form" : form,
+			   }
+	return render(request, "skroc_url/inputURL.html", context)	
