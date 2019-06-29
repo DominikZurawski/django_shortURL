3de0f3834f527439ff33fa8b939d4e2f85e8eb4f
DominikZurawski <dominikzurawski15@gmail.com>
2019-06-29 12:49:37 +0200

    heroku

--- short_URL/settings.py
+++ short_URL/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '27d2!0i*anyjap7=z9e33gch&fvf88*+53oz_l-&geqa_f@-_-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
